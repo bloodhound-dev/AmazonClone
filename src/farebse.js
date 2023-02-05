8f358faa3ac107c94079f8f22bb8856b94425ca0
Rishab Raj <rishabrajmishra@gmail.com>
2023-02-05 21:28:23 +0530

    first commit

---
+++ src/farebse.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBR_FKr4vs-Snu9zN-LQWHuwcziHpjvMUY",
+    authDomain: "clone-c6d4a.firebaseapp.com",
+    projectId: "clone-c6d4a",
+    storageBucket: "clone-c6d4a.appspot.com",
+    messagingSenderId: "855870349475",
+    appId: "1:855870349475:web:f920f7c34adeb4fce81f16",
+    measurementId: "G-G8FJBYKHRN"
+  };
